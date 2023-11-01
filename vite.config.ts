066f3a546cf510957890641a78c5061443dcadb4
Lior <liorvainer@gmail.com>
2023-11-01 12:48:13 +0200

    fix(config): updated tsconfig.json and vite config file to be suited better

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+    strictPort: true,
+    https: false,
+    host: "localhost",
+  },
 });
